bf6b8a29a3d10fdd42af65e4e3e731434a67ec06
mariaraspopova <mariarass2001@gmail.com>
2025-01-21 12:10:02 -0700

    fix: config

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import { HTMLAttributes, ReactChild } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: ReactChild;
