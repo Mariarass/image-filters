025e44ccaaeac730d33149f11ac93a69766df792
mariaraspopova <mariarass2001@gmail.com>
2025-02-19 15:50:27 -0700

    fix: styles props

--- src/ImageFilters.tsx
+++ src/ImageFilters.tsx
@@ -397,7 +397,7 @@ const WebGLImageFilter: React.FC<FilterProps> = ({
 
     console.log(savedImage)
     return (
-        <div style={{ position: 'absolute', width: '100%', height: '100%',overflow: 'hidden', ...styles }}>
+        <div style={{ position: 'relative', width: '100%', height: '100%',overflow: 'hidden', ...styles }}>
 
             <canvas
                 ref={canvasRef}
